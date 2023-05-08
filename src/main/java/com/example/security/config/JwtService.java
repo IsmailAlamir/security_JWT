06a09b88c45a4bd7b78b16ffe1c15d7f06d25ea5
IsmailAlamir <ismaeelalamir@gmail.com>
2023-05-08 19:57:48 +0000

    security

--- src/main/java/com/example/security/config/JwtService.java
+++ src/main/java/com/example/security/config/JwtService.java
@@ -2,11 +2,16 @@ package com.example.security.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
 
 @Service // help to manage beans
 public class JwtService {
@@ -16,8 +21,29 @@ public class JwtService {
     // min = 256 bit or more , hex
     // you can add it to propagates
     public String extractUsername(String token) {
-        return null;
+
+        return extractClaim(token, Claims::getSubject);
     }
+
+    public <T> T extractClaim(String token , Function<Claims, T> claimsResolver){
+        final Claims claims=extractAllClaims(token); //extract the claims
+        return claimsResolver.apply(claims);
+    }
+    public String generateToken(
+
+            Map<String, Object> extractClaims,
+            UserDetails userDetails
+    ){
+        return Jwts
+                .builder()
+                .setClaims(extractClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+1000*60*24))
+                .signWith(getSignKey(), SignatureAlgorithm.HS256)
+                .compact()
+    }
+
     private Claims extractAllClaims (String token){ //extract  jwt
         return Jwts
             .parserBuilder()
